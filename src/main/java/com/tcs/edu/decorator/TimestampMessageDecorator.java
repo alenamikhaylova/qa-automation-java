d5b743e3027f5881d4ec48ea9e4626207d72f614
al.v.mikhaylova <al.v.mikhaylova@tinkoff.ru>
2022-03-24 23:17:59 +0300

    feat: add class TimestampMessageDecorator with javadoc

---
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -0,0 +1,22 @@
+package com.tcs.edu.decorator;
+
+import java.time.Instant;
+
+/**
+ * назначение класса: декорирование сообщений с операцией добавления к строке текущего времени
+ *
+ * @author Михайлова Алена Владимировна
+ */
+public class TimestampMessageDecorator {
+
+    /**
+     * назначение метода: вывод на экран/консоль строки сообщения, обогащенной текущим временем,
+     * <br>входные параметры: данные с типом String (Строка),
+     * <br>побочные эффекты: осуществление операций вывода, изменение консоли
+     *
+     * @param message сообщение с типом String
+     */
+    public static void decorate(String message) {
+        System.out.println(Instant.now() +" "+ message);
+    }
+}
