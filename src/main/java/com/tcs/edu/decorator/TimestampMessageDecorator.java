29f1829bd41f24c09b43f6b72e70662826016087
Alena <101282394+alenamikhaylova@users.noreply.github.com>
2022-04-04 00:31:18 +0300

    Training 3 (#4)
    * docs: add javadoc to class ConsolePrinter and to void print
    
    * docs: add package to class ConsolePrinter and to class Application
    
    * feat: add class TimestampMessageDecorator with javadoc
    
    * docs: add local variable decoratedMessage
    
    * fix: replace String decorate in class TimestampMessageDecorator (training #3-3)
    
    * add: Local variable decoratedMessage in void TimestampMessageDecorator.decorate (training #4-1)
    
    * add: Global variable messageCount in class TimestampMessageDecorator (training #4-2)
    
    * add: type of variable decoratedMessage (training #4-3)
    
    * add: final modifier to variable decoratedMessage (training #4-4)

---
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -0,0 +1,29 @@
+package com.tcs.edu.decorator;
+
+import java.time.Instant;
+
+/**
+ * назначение класса: декорирование сообщений с операцией добавления к строке текущего времени
+ *
+ * @author Михайлова Алена Владимировна
+ */
+public class TimestampMessageDecorator {
+
+    /**
+     * Счетчик
+     */
+    public static Integer messageCount = 0;
+
+    /**
+     * назначение метода: вывод на экран/консоль строки сообщения, обогащенной текущим временем,
+     * <br>входные параметры: данные с типом String (Строка),
+     * <br>Side effect: осуществление операций вывода, изменение консоли
+     *
+     * @param message сообщение с типом String
+     */
+    public static String decorate(final String message) {
+        final var decoratedMessage = Instant.now() + " " + message;
+        messageCount++;
+        return messageCount + " " + decoratedMessage;
+    }
+}
