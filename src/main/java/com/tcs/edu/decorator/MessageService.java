65f498846b874c1446311f0b6b9cd6132dfe0567
al.v.mikhaylova <al.v.mikhaylova@tinkoff.ru>
2022-05-08 13:27:25 +0300

    fix: add void checkDifferent and checkDescend into class MessageService (training #7)

--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -1,13 +1,8 @@
 package com.tcs.edu.decorator;
 
-import com.tcs.edu.printer.ConsolePrinter;
-
-import java.util.Objects;
-
-import static com.tcs.edu.decorator.Doubling.*;
-import static com.tcs.edu.decorator.MessageOrder.*;
 import static com.tcs.edu.decorator.SeverityDecorator.mapToString;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
+import static com.tcs.edu.printer.ConsolePrinter.print;
 
 /**
  * API процедуры вывода сообщений с уровнем важности
@@ -22,64 +17,82 @@ public class MessageService {
      * @param doubling признак дублирования сообщений
      * @param messages массив сообщений с типом String
      */
-    public static void log(Severity level, MessageOrder order, Doubling doubling, String message, String... messages) {
-        String[] arrayOfMessages = new String[messages.length + 1];
-
-        if (doubling == DISTINCT) {
-            if (Objects.equals(order.name(), "DESC")) {
-                for (int i = messages.length - 1; i >= 0; i--) {
-                    if (messages[i] != null & !MessageService.insideArray(messages[i], arrayOfMessages)) {
-                        ConsolePrinter.print(decorate(messages[i] + " " + mapToString(level)));
-                        arrayOfMessages[i] = messages[i];
-                    }
-                }
-                if (!MessageService.insideArray(message, arrayOfMessages)) {
-                    ConsolePrinter.print(decorate(message + " " + mapToString(level)));
-                }
+    public static void log(Severity level, MessageOrder order, Doubling doubling, String... messages) {
+        if (doubling != null) {
+            if (doubling.equals(Doubling.DOUBLES)) {
+                log(level, order, messages);
+            } else if (doubling.equals(Doubling.DISTINCT)) {
+                log(level, order, checkDifferent(messages));
             }
-            if (order == ASC) {
-                ConsolePrinter.print(decorate(message + " " + mapToString(level)));
-                arrayOfMessages[messages.length] = message;
+        }
+    }
 
-                for (int i = 0; i <= messages.length - 1; i++) {
-                    if (message != null & messages[i] != null & !MessageService.insideArray(messages[i], arrayOfMessages)) {
-                        ConsolePrinter.print(decorate(messages[i] + " " + mapToString(level)));
-                        arrayOfMessages[i] = messages[i];
-                    }
-                }
+    public static void log(Severity level, MessageOrder messageOrder, String... messages) {
+        if (messageOrder != null) {
+            if (messageOrder.equals(MessageOrder.ASC)) {
+                log(level, messages);
+            } else if (messageOrder.equals(MessageOrder.DESC)) {
+                log(level, checkDescend(messages));
             }
-        } else if (doubling == DOUBLES) {
-            if (Objects.equals(order.name(), "DESC")) {
-                for (int i = messages.length - 1; i >= 0; i--) {
-                    if (message != null & messages[i] != null) {
-                        ConsolePrinter.print(decorate(messages[i] + " " + mapToString(level)));
-                    }
-                }
-                ConsolePrinter.print(decorate(message + " " + mapToString(level)));
-            }
-            if (order == ASC) {
-                ConsolePrinter.print(decorate(message + " " + mapToString(level)));
-                arrayOfMessages[messages.length] = message;
+        }
+    }
 
-                for (int i = 0; i <= messages.length - 1; i++) {
-                    if (message != null & messages[i] != null) {
-                        ConsolePrinter.print(decorate(messages[i] + " " + mapToString(level)));
-                    }
+    public static void log(Severity level, String... messages) {
+        if (level != null && messages != null && messages.length != 0) {
+            for (String currentMessage : messages) {
+                if (currentMessage != null) {
+                    print(decorate(String.format("%s %s", currentMessage, mapToString(level))));
                 }
             }
         }
     }
 
     /**
+     * Назначение: вывод неповторяющихся сообщений
+     *
+     * @param messages массив сообщений
+     * @return возвращает массив неповторящющихся сообщений
+     */
+    public static String[] checkDifferent(String... messages) {
+        String[] messagesDifferent = new String[messages.length];
+        if (messages.length != 0) {
+            messagesDifferent[0] = messages[0];
+            int k = 1;
+            for (int i = 1; i < messages.length; i++) {
+                if (!checkInsideArray(messages[i], messagesDifferent)) {
+                    messagesDifferent[k] = messages[i];
+                    k++;
+                }
+            }
+        }
+        return messagesDifferent;
+    }
+
+    /**
+     * Назначение: вывод сообщений в убывающем порядке
+     *
+     * @param messages массив сообщений
+     * @return возвращает массив сообщений в убывающем порядке
+     */
+    private static String[] checkDescend(String... messages) {
+        int k = messages.length;
+        String[] messagesDescend = new String[k];
+        for (int i = 0; i < k; i++) {
+            messagesDescend[i] = messages[k - i - 1];
+        }
+        return messagesDescend;
+    }
+
+    /**
      * Назначение: проверка наличия сообщения в массиве
      *
-     * @param message        сообщение с типом String
-     * @param messageInArray массив сообщений с типом String
-     * @return возвращает булево значение
+     * @param message  сообщение с типом String
+     * @param messages массив сообщений
+     * @return возвращает булево значение: message в массиве messages -> true, иначе -> false
      */
-    private static boolean insideArray(String message, String[] messageInArray) {
-        for (String arrayOfMessagesPrint : messageInArray) {
-            if (Objects.equals(message, arrayOfMessagesPrint)) {
+    public static boolean checkInsideArray(String message, String[] messages) {
+        for (String s : messages) {
+            if (s != null && s.equals(message)) {
                 return true;
             }
         }
