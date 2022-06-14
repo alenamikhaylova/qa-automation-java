3ef748085844d7efa315dc7574a97a092a3914f5
al.v.mikhaylova <al.v.mikhaylova@tinkoff.ru>
2022-06-14 23:38:29 +0300

    fix: replace values into each test (training #12) add testcases for api (training #13)

--- src/test/java/ApiDemoTest.java
+++ src/test/java/ApiDemoTest.java
@@ -1,12 +1,17 @@
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+/**
+ * Test Cases for check API on localhost
+ */
 public class ApiDemoTest {
     @BeforeAll
+    @DisplayName("Authentication")
     public static void setUpAuth() {
         PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
         authScheme.setUserName("admin");
@@ -20,6 +25,7 @@ public class ApiDemoTest {
     }
 
     @Test
+    @DisplayName("Get locations of city")
     public void shouldGetLocationsWhenPopulateDb() {
         when()
                 .get("/api/locations")
@@ -33,16 +39,51 @@ public class ApiDemoTest {
     }
 
     @Test
+    @DisplayName("Create new country if it doesn't exist")
     public void shouldCreateCountryWhenUnique() {
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        " \"countryName\": \"EE\"\n" +
+                        " \"countryName\": \"MI\"\n" +
                         "}")
                 .when()
                 .post("/api/countries")
                 .then()
                 .statusCode(201)
                 .body("id", not(empty()));
+    }
+
+    @Test
+    @DisplayName("Update name of country")
+    public void shouldUpdateCountry() {
+        given()
+                .contentType("application/json")
+                .body("{\n" +
+                        " \"id\": \"3\",\n" +
+                        " \"countryName\": \"FE\"\n" +
+                        "}")
+                .when()
+                .put("/api/countries/3")
+                .then()
+                .statusCode(200)
+                .body("id", is(3),
+                        "countryName", is("FE")
+                );
+    }
+
+    @Test
+    @DisplayName("Delete country")
+    public void shouldDeleteCountry() {
+        given()
+                .contentType("application/json")
+                .body("{\n" +
+                        " \"id\": \"3\",\n" +
+                        " \"countryName\": \"FE\"\n" +
+                        "}")
+                .when()
+                .delete("api/countries/3")
+                .then()
+                .statusCode(204)
+                .body(isEmptyOrNullString());
     }
 }
